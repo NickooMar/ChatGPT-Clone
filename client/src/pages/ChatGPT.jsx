fa81e136a4ec7a2aae79736f737817ec252d5936
Nicolás <nicoo.marsili@gmail.com>
2023-02-03 13:30:36 -0300

    ChatGPT Default Screen with Examples, Capabilities and Limitations

--- client/src/pages/ChatGPT.jsx
+++ client/src/pages/ChatGPT.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 
 import SendIcon from "@mui/icons-material/Send";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import FlashOnIcon from "@mui/icons-material/FlashOn";
+import ReportProblemIcon from "@mui/icons-material/ReportProblem";
+
 import { chat_userAI_logo, default_user_logo } from "../assets";
 
 const ChatGPT = () => {
@@ -48,12 +52,105 @@ const ChatGPT = () => {
     <div className="w-full h-screen bg-[#E5E4E2] flex flex-col justify-center items-center relative">
       <div className=" h-full w-7/12 mt-4  overflow-x-hidden overflow-y-auto">
         {responseArray.length === 0 ? (
-          <h1
-            className="text-4xl text-center font-bold"
-            style={{ fontFamily: "montserrat" }}
-          >
-            ChatGPT
-          </h1>
+          <div className="w-full h-full">
+            <h1
+              className="text-4xl text-center font-bold"
+              style={{ fontFamily: "montserrat" }}
+            >
+              ChatGPT
+            </h1>
+            <div className="hidden lg:grid lg:grid-flow-col lg:auto-cols-auto lg:place-items-center gap-4 mt-4">
+              <div className="flex flex-col items-center">
+                <WbSunnyIcon className="w-12 h-12" />
+                <h1
+                  style={{ fontFamily: "montserrat" }}
+                  className="text-lg font-semibold"
+                >
+                  Examples
+                </h1>
+
+                <div className="grid grid-rows-3">
+                  <div className="bg-slate-600 w-full rounded-md p-3 text-white mt-2">
+                    <button
+                      onClick={() =>
+                        setPrompt("Explain quantum computing in simple terms")
+                      }
+                    >
+                      <h1>"Explain quantum computing in simple terms" → </h1>
+                    </button>
+                  </div>
+                  <div className="bg-slate-600 w-full rounded-md p-3 text-white mt-2">
+                    <button
+                      onClick={() =>
+                        setPrompt(
+                          "Got any creative ideas for a 10 year old's birthday?"
+                        )
+                      }
+                    >
+                      <h1>
+                        "Got any creative ideas for a 10 year old's birthday?" →{" "}
+                      </h1>
+                    </button>
+                  </div>
+                  <div className="bg-slate-600 w-full rounded-md p-3 text-white mt-2">
+                    <button
+                      onClick={() =>
+                        setPrompt("How do i make an HTTP request in javascript")
+                      }
+                    >
+                      <h1>"How do i make an HTTP request in javascript" → </h1>
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-col items-center">
+                <FlashOnIcon className="w-12 h-12" />
+                <h1
+                  style={{ fontFamily: "montserrat" }}
+                  className="text-lg font-semibold"
+                >
+                  Capabilities
+                </h1>
+
+                <div className="grid grid-rows-3">
+                  <div className="bg-slate-600 w-full rounded-md p-3 text-white mt-2">
+                    <h1>
+                      "Remembers what user said earlier in the conversation"
+                    </h1>
+                  </div>
+                  <div className="bg-slate-600 w-full rounded-md p-3 text-white mt-2">
+                    <h1>"Allows user to provide follow-up corrections"</h1>
+                  </div>
+                  <div className="bg-slate-600 w-full rounded-md p-3 text-white mt-2">
+                    <h1>"Trained to decline inappropriate requests"</h1>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-col items-center">
+                <ReportProblemIcon className="w-12 h-12" />
+                <h1
+                  style={{ fontFamily: "montserrat" }}
+                  className="text-lg font-semibold"
+                >
+                  Limitations
+                </h1>
+
+                <div className="grid grid-rows-3">
+                  <div className="bg-slate-600 w-full rounded-md p-3 text-white mt-2">
+                    <h1>"May occasionally generate incorrect information"</h1>
+                  </div>
+                  <div className="bg-slate-600 w-full rounded-md p-3 text-white mt-2">
+                    <h1>"May occasionaly produce harmful instructions"</h1>
+                  </div>
+                  <div className="bg-slate-600 w-full rounded-md p-3 text-white mt-2">
+                    <h1>"Limited knowledge of world and events after 2021"</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         ) : isLoading ? (
           <div className="h-full flex justify-center items-center">
             <h1 className="text-3xl animate-ping">...</h1>
